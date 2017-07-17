24e6b6e03bd1f43e07ef086435e3df411a75cf43
RavindraSinghKhichi <ravindrasinghkhichi@gmail.com>
2017-07-17 11:06:18 +0530

    added kitchen and chef spec concepts

--- cookbooks/generator_concepts/recipes/default.rb
+++ cookbooks/generator_concepts/recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+include_recipe 'java'
+package 'httpd' do
+  action :install
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
--- cookbooks/generator_concepts/spec/unit/recipes/default_spec.rb
+++ cookbooks/generator_concepts/spec/unit/recipes/default_spec.rb
@@ -7,16 +7,26 @@
 require 'spec_helper'
 
 describe 'generator_concepts::default' do
-  context 'When all attributes are default, on an Ubuntu 16.04' do
+  context 'CentOs' do
     let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.3.1611')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
+    end
+
+    it 'installs httpd' do
+      expect(chef_run).to install_package('httpd')
+    end
+
+    it 'enables the httpd' do
+      expect(chef_run).to enable_service('httpd')
+    end
+
+    it 'starts the httpd service' do
+      expect(chef_run).to start_service('httpd')
     end
   end
 end
